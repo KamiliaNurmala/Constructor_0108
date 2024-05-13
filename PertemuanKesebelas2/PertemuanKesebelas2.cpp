2e65bdb380616c7134fb09236bfd4417a0ac9eb7
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-05-13 11:02:17 +0700

    memberi value pada nim

--- PertemuanKesebelas2/PertemuanKesebelas2.cpp
+++ PertemuanKesebelas2/PertemuanKesebelas2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class mahasiswa {
 private:
-	static int nim;
+	static long long int nim;
 public:
 	int id;
 	string nama;
@@ -17,3 +17,5 @@ public:
 
 	mahasiswa(string pnama) : nama(pnama) { setID(); }
 };
+
+long long int mahasiswa::nim = 20230140108;
